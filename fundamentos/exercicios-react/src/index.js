b6da134a23f995b7d241373ed123c467cf38be7e
Lucas Souza <lucas.souza@devplace.com.br>
2019-05-08 09:32:19 -0300

    Adição de primeiro componente;

--- fundamentos/exercicios-react/src/index.js
+++ fundamentos/exercicios-react/src/index.js
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
 
 const elemento = document.getElementById('root')
 
-ReactDOM.render(<h1>Olá React</h1>, elemento)
+ReactDOM.render(
+<div>
+    <PrimeiroComponente />
+</div>
+, elemento)
