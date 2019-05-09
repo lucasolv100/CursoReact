1c45fb7cd82c200427d16c3d904fca30b71e2105
Lucas Souza <lucas.souza@devplace.com.br>
2019-05-09 09:32:56 -0300

    adicionado dois componentes;

--- fundamentos/exercicios-react/src/index.js
+++ fundamentos/exercicios-react/src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
+/* import PrimeiroComponente from './componentes/PrimeiroComponente' */
+import {CompA, CompB} from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
 <div>
-    <PrimeiroComponente texto={"Olá React"} />
-</div>
+    {/* <PrimeiroComponente texto={"Olá React"} /> */}
+    <CompA valor={"componente a"}/>
+    <CompB valor={"componente b"}/>
+    </div>
 , elemento)
