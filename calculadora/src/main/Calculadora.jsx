351c30e45e4d30336b9a191d2925b5c27b2d2114
Lucas Souza <lucas.souza@devplace.com.br>
2019-05-17 09:09:07 -0300

    Iniciado projeto de criação de calculadora;

---
+++ calculadora/src/main/Calculadora.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Calculadora.css'
+import Button from '../components/Button'
+
+export default class Calculadora extends Component {
+    render() {
+        return (
+            <div className='calculadora'>
+                <Button />
+            </div>
+        )
+    }
+}
