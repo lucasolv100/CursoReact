a8eb2de124f811fd76ff562b0c42598c7d49bd5f
Lucas Souza <lucas.souza@devplace.com.br>
2019-06-24 09:06:03 -0300

    inicio de integração com redux;

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Field from './field'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
 
 ReactDOM.render(
-    <Field initialValue="Teste" />
+    <Provider store={createStore(reducers)}>
+        <Field initialValue="Teste" />
+    </Provider>
     , document.getElementById('app')
 )
