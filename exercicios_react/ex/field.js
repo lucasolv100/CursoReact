97fe9ffdd31097ec264c0bdfdbae02b43e26f202
Lucas Souza <lucas.souza@devplace.com.br>
2019-06-26 09:17:33 -0300

    iniciado bind dispatch

--- exercicios_react/ex/field.js
+++ exercicios_react/ex/field.js
@@ -1,23 +1,28 @@
-import React, {Component} from 'react'
-import connect from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldactions'
 
 class Field extends Component {
-    
-    render()
-    {
+
+    render() {
         return (
             <div>
                 <label>{this.props.value}</label>
-                <input onChange={this.handleChange} value={this.props.value} />
+                <input onChange={this.props.changeValue} value={this.props.value} />
             </div>
         )
     }
 }
 
-function mapStateToProps(state){
-    return{
+function mapStateToProps(state) {
+    return {
         value: state.field.value
     }
 }
 
-export default connect(mapStateToProps)(Field)
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({changeValue}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
