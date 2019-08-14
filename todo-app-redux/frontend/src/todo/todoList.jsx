ba67318f1b77a106a81837ec058239fb40679603
Lucas Souza <lucas.souza@devplace.com.br>
2019-08-14 15:50:53 -0300

    Adicionado reducers para listagem de tarefas; Adicionado middleware para promises em reducers;

--- todo-app-redux/frontend/src/todo/todoList.jsx
+++ todo-app-redux/frontend/src/todo/todoList.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 import IconButton from '../template/iconButton'
+import { connect } from 'react-redux'
 
-export default props => {
+const TodoList = props => {
 
-    const renderRows = () => 
-    {
+    const renderRows = () => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton 
-                    style='success'
-                    icon='check'
-                    hide={todo.done}
-                    onClick={() => props.handleMarkAsDone(todo)}/>
-                    <IconButton 
-                    style='warning'
-                    icon='undo'
-                    hide={!todo.done}
-                    onClick={() => props.handleMarkAsPending(todo)} />
-                    <IconButton 
-                    style='danger'
-                    icon='trash-o'
-                    hide={!todo.done}
-                    onClick={() => props.handleRemove(todo)}
+                    <IconButton
+                        style='success'
+                        icon='check'
+                        hide={todo.done}
+                        onClick={() => props.handleMarkAsDone(todo)} />
+                    <IconButton
+                        style='warning'
+                        icon='undo'
+                        hide={!todo.done}
+                        onClick={() => props.handleMarkAsPending(todo)} />
+                    <IconButton
+                        style='danger'
+                        icon='trash-o'
+                        hide={!todo.done}
+                        onClick={() => props.handleRemove(todo)}
                     ></IconButton></td>
             </tr>
         ))
     }
 
     return (
-    <table className="table">
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th className="tableActions">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            {renderRows()}
-        </tbody>
-    </table>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th className="tableActions">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
     )
 }
+
+const mapStateToProps = state => ({list: state.todo.list})
+
+export default connect(mapStateToProps)(TodoList)
