9324171709973998fed57a56dfdecd1a6e150013
Lucas Souza <lucas.souza@devplace.com.br>
2019-08-14 12:03:30 -0300

    Adicionado app com migração para react-redux

---
+++ todo-app-redux/frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader';
+
+export default props =>(
+    <div>
+        <PageHeader name="About" small="Us" />
+        <h2>
+            Our history
+        </h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac libero dui. Mauris et efficitur justo. Sed efficitur a lorem et luctus. Vestibulum viverra, tortor quis pretium hendrerit, mi turpis dignissim neque, id auctor diam mi sed eros. Duis tristique, ante vel varius lacinia, lectus purus convallis sapien, non tincidunt orci ex ut orci. Nam in massa in arcu eleifend gravida. Aenean vel velit at magna suscipit aliquet nec et diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus elementum purus at venenatis dictum. Nullam ut bibendum arcu. Cras aliquet faucibus purus, nec convallis massa luctus a.
+        </p>
+    </div>
+)
